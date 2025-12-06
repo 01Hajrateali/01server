ec8a456f0398fe9be1999dc4f2a689b4fa6c83fd
01Hajrateali <01hajrateali@gmail.com>
2025-12-06 10:45:10 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,20 +4,6 @@ const fs = require('fs');
 
 const routes = express.Router();
 
-routes.post('/H01', (req, res) => {
-  const f = path.join('../db/image.json');
-  fs.readFile(f, 'utf8', (err, od) => {
-    
-    if(err) return res.status(500).send("reading failed");
-    
-    let data = [];
-    data = JSON.parse(od);
-    data.push(req.body);
-    
-    fs.writeFileSync(f, JSON.stringify(data, null, 2));
-    res.send("Success full ")
-  });
-});
 
 
 module.exports = routes;
